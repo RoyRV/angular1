133069aa0d5a00b5e8c6236939da666cf04baed1
royrv <roy.rojval@gmail.com>
2017-12-21 09:03:48 -0500

    first commit

---
+++ app/app.js
@@ -0,0 +1,15 @@
+﻿ angular.module('bluprint', [
+     'ngRoute',
+     'bluprint.home',
+ ])
+.config(function ($routeProvider, $httpProvider) {
+    $routeProvider
+        .when('/', {
+            controller: 'HomeCtrl',
+            templateUrl: 'app/components/home/home.html'
+        });
+
+})
+.run(function () {
+    
+ });
---
+++ app/components/home/home.js
@@ -0,0 +1,4 @@
+﻿angular.module('bluprint.home', [])
+.controller('HomeCtrl', function ($scope) {
+    $scope.welcomeMessage = "Bluprint Report";
+});
